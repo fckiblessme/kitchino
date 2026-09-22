63cabd54b8e5380bfdd2fa80f506764483a7f8cb
DarSkry <daryaskry@gmail.com>
2026-09-22 10:18:53 +0300

    Регистрация таблиц и DAO склада

--- app/src/main/java/com/kitchino/app/data/AppDatabase.kt
+++ app/src/main/java/com/kitchino/app/data/AppDatabase.kt
@@ -5,6 +5,16 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+import com.kitchino.app.warehouse.data.dao.ConsumptionLogDao
+import com.kitchino.app.warehouse.data.dao.IngredientDao
+import com.kitchino.app.warehouse.data.dao.RecipeDao
+import com.kitchino.app.warehouse.data.entity.ConsumptionLogEntity
+import com.kitchino.app.warehouse.data.entity.IngredientEntity
+import com.kitchino.app.warehouse.data.entity.RecipeEntity
+import com.kitchino.app.warehouse.data.entity.RecipeIngredientEntity
+import com.kitchino.app.warehouse.data.dao.IngredientBatchDao
+import com.kitchino.app.warehouse.data.entity.IngredientBatchEntity
+
 import androidx.room.TypeConverters
 import com.kitchino.app.dishbatch.data.Converters
 import com.kitchino.app.dishbatch.data.DiscountDecisionDao
@@ -13,11 +23,17 @@ import com.kitchino.app.dishbatch.data.DishBatchDao
 import com.kitchino.app.dishbatch.data.DishBatchEntity
 
 
-@Database(entities = [DishBatchEntity::class, DiscountDecisionEntity::class], version = 1)
+
+@Database(entities = [DishBatchEntity::class, DiscountDecisionEntity::class,  IngredientEntity::class, IngredientBatchEntity::class, RecipeEntity::class, RecipeIngredientEntity::class,ConsumptionLogEntity::class], version = 1)
 @TypeConverters(Converters::class)
 abstract class AppDatabase  : RoomDatabase(){
     abstract fun returnDishBatchDao() : DishBatchDao
     abstract fun returnDiscountDecisionDao() : DiscountDecisionDao
+
+    abstract fun returnIngredientDao(): IngredientDao
+    abstract fun returnIngredientBatchDao(): IngredientBatchDao
+    abstract fun returnRecipeDao(): RecipeDao
+    abstract fun returnConsumptionLogDao(): ConsumptionLogDao
 
     companion object{
         private var instance: AppDatabase? = null
