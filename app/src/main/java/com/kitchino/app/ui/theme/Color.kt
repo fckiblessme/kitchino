7a89b26236e2f11496193e7ca1abf4e8feed3c6a
fckiblessme <wcbrtidkw@gmail.com>
2026-09-24 14:16:33 +0300

    Добавлен цвет карточки по типу решения

--- app/src/main/java/com/kitchino/app/ui/theme/Color.kt
+++ app/src/main/java/com/kitchino/app/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LightGreen = Color(0xFFB8D18F)
+val LightOrange = Color(0xFFF6972D)
+val LightRed = Color(0xFFFFE34C)
